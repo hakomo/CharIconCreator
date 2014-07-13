76dd03f6352ce5bbe1f7ee31956789757d6f2775
hakomo <hakomof@gmail.com>
2014-07-13 16:20:56 +0900

    extend image

--- img.py
+++ img.py
@@ -12,5 +12,10 @@ def createicon(s, c):
     img.save('i32.png')
 
 # s, c = 'F', (64, 96, 128)
-s, c = 'R', (212, 255, 0)
-createicon(s, c)
+# s, c = 'R', (212, 255, 0)
+# createicon(s, c)
+
+img = Image.new('RGBA', (300, 200))
+ImageDraw.Draw(img).text((118, 92), '開発中！',
+    font=ImageFont.truetype('meiryo.ttc', 16, encoding='utf-8'), fill=(0, 0, 0))
+img.save('dev.png')
